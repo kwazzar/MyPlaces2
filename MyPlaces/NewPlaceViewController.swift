d158c741fcb80f967b5dcc6d7c91e8f529bb65f7
kwazzar <fugitiwe@gmail.com>
2022-03-05 03:11:36 +0200

    pass data between Controllers

---
+++ MyPlaces/NewPlaceViewController.swift
@@ -0,0 +1,134 @@
+//
+//  NewPlaceViewController.swift
+//  MyPlaces
+//
+//  Created by Quasar on 01.03.2022.
+//
+
+
+import UIKit
+
+class NewPlaceViewController: UITableViewController {
+    
+    var newPlace: Place?
+    var imageIsChanged = false
+    
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    
+    @IBOutlet weak var placeImage: UIImageView!
+    @IBOutlet weak var placeName: UITextField!
+    @IBOutlet weak var placeLocation: UITextField!
+    @IBOutlet weak var placeType: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        tableView.tableFooterView = UIView()
+        
+        saveButton.isEnabled = false
+        
+        placeName.addTarget(self, action: #selector(textFieldChanged), for: .editingChanged)
+
+    }
+
+    // Mark: Table view delegate
+     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+         if indexPath.row == 0 {
+             
+             let cameraIcon = UIImage(named: "camera")
+             let photoIcon = UIImage(named: "photo")
+             
+             let actionSheet = UIAlertController(title: nil,
+                                                 message: nil,
+                                                 preferredStyle: UIAlertController.Style.actionSheet)
+             
+             let camera = UIAlertAction(title: "Camera", style: .default) { _ in
+                 self.chooseImagePicker(source: .camera)
+            }
+             camera.setValue(cameraIcon, forKey: "image")
+             camera.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
+             
+             let photo = UIAlertAction(title: "Photo", style: .default) { _ in
+                 self.chooseImagePicker(source: .photoLibrary)
+             }
+             
+             photo.setValue(photoIcon, forKey: "image")
+             photo.setValue(CATextLayerAlignmentMode.left, forKey: "titleTextAlignment")
+             
+             let cancel = UIAlertAction(title: "Cancel", style: .cancel)
+
+             actionSheet.addAction(camera)
+             actionSheet.addAction(photo)
+             actionSheet.addAction(cancel)
+             
+             present(actionSheet, animated: true)
+         } else {
+             view.endEditing(true)
+         }
+}
+    
+    func saveNewPlace() {
+        
+        var image: UIImage?
+        
+        if imageIsChanged {
+            image = placeImage.image
+        } else {
+            image = UIImage(named: "imagePlaceholder")
+        }
+        
+        newPlace = Place(name: placeName.text!, location: placeLocation.text, type: placeType.text, image: image, restaurantImage: nil)
+        
+    }
+    
+    @IBAction func cancelAction(_ sender: UIBarButtonItem) {
+        dismiss(animated: true)
+    }
+}
+ 
+// MARK: Text field delegate
+
+extension NewPlaceViewController: UITextViewDelegate {
+    
+    // Скрываем клавиатуру по нажатии на Done
+
+    func textFieldSghouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    @objc private func textFieldChanged() {
+        
+        if placeName.text?.isEmpty == false {
+            saveButton.isEnabled = true
+        } else {
+            saveButton.isEnabled = false
+        }
+    }
+}
+
+//MARK: Work with image
+extension NewPlaceViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    func chooseImagePicker(source: UIImagePickerController.SourceType) {
+        
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = source
+            present(imagePicker, animated: true)
+        }
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        placeImage.image = info[.editedImage] as? UIImage
+        placeImage.contentMode = .scaleAspectFill
+        placeImage.clipsToBounds = true
+        
+        imageIsChanged = true
+        
+        dismiss(animated: true)
+    }
+}
---
+++ MyPlaces/PlaceModel.swift
@@ -0,0 +1,36 @@
+//
+//  PlaceModel.swift
+//  MyPlaces
+//
+//  Created by Quasar on 23.02.2022.
+//
+
+import UIKit
+
+struct Place {
+    
+    var name: String
+    var location: String?
+    var type: String?
+    var image: UIImage?
+    var restaurantImage: String?
+    
+       static let restaurantNames = [
+        "Burger Heroes", "Kitchen", "Bonsai", "Дастархан",
+        "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes",
+        "Speak Easy", "Morris Pub", "Вкусные истории",
+        "Классик", "Love&Life", "Шок", "Бочка"
+        ]
+    
+   static func getPlaces() -> [Place] {
+        
+        var places = [Place]()
+        
+        for place in restaurantNames {
+            places.append(Place(name: place, location: "Уфа", type: "Ресторан", image: nil, restaurantImage: place))
+        }
+        
+        return places
+    }
+    
+}
